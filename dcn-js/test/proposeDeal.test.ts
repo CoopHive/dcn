341aa87bf92a467035caf1e01f2386cc7aeb4f28
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-08-26 17:31:55 -0600

    mock approvals updated block height for sepolia balances

--- dcn-js/test/proposeDeal.test.ts
+++ dcn-js/test/proposeDeal.test.ts
@@ -3,7 +3,9 @@ import {describe, expect, test} from '@jest/globals';
 import { fileURLToPath  } from 'url';
 import * as path from 'node:path'
 import { Client, AgentType } from '../src/Client'
-import { zeroAddress } from 'viem'
+import { zeroAddress, getContract, createWalletClient, http, parseEther, createPublicClient  } from 'viem'
+import { localhost, baseSepolia } from 'viem/chains'
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts'
 import * as MockERC20 from '../src/artifacts/baseSepolia/ERC20Mock.json'
 
 import type { BuyerAttest } from '../src/message-schema';
@@ -18,28 +20,56 @@ describe("proposeDeal", async  () => {
     if (!process.env.PRIVATE_KEY_BUYER || !process.env.PRIVATE_KEY_SELLER) {
       throw new Error("Please set PRIVATE_KEY_BUYER and PRIVATE_KEY_SELLER env variable")
     }
+
+    const deployer = privateKeyToAccount(process.env.PRIVATE_KEY_BUYER as `0x${string}`)
+    const walletClient = createWalletClient({
+      account: deployer,  
+      chain: baseSepolia,
+      transport: http(`http://127.0.0.1:8545`),
+    })
+    const publicClient = createPublicClient({
+      chain: baseSepolia,
+      transport: http(`http://127.0.0.1:8545`),
+    })
+
+
+
+
+
+    const erc20 = getContract({
+      address: MockERC20.address as `0x${string}`, 
+      abi: MockERC20.abi,
+      client: {wallet: walletClient}
+    })
+
     const buyersClient = await new Client({
       role:  AgentType.BUYER,
-      rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
-      //rpcUrl: `http://127.0.0.1:8545`,
+      //rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
+      rpcUrl: `http://127.0.0.1:8545`,
       redisUrl: `redis://127.0.0.1:6379`,
       privateKey: process.env.PRIVATE_KEY_BUYER as `0x${string}`,
     })
+    let hash =  await erc20.write.mint([buyersClient.account.address, parseEther('100')]);
+    publicClient.waitForTransactionReceipt({hash})
     
     const sellersClient = await new Client({
       role: AgentType.SELLER,
-      rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
-      //rpcUrl: `http://127.0.0.1:8545`,
+      //rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
+      rpcUrl: `http://127.0.0.1:8545`,
       redisUrl: `redis://127.0.0.1:6379`,
       privateKey: process.env.PRIVATE_KEY_SELLER as `0x${string}`,
     })
+    hash = await erc20.write.mint([sellersClient.account.address, parseEther('100')]);
+    publicClient.waitForTransactionReceipt({hash})
     const validatorClient = await new Client({
       role: AgentType.VALIDATOR,
-      rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
-      //rpcUrl: `http://127.0.0.1:8545`,
+      //rpcUrl: `https://site1.moralis-nodes.com/base-sepolia/${process.env.MORALIS}`,
+      rpcUrl: `http://127.0.0.1:8545`,
       redisUrl: `redis://127.0.0.1:6379`,
       privateKey: process.env.PRIVATE_KEY_VALIDATOR as `0x${string}`,
     })
+    hash = await erc20.write.mint([validatorClient.account.address, parseEther('100')]);
+    publicClient.waitForTransactionReceipt({hash})
     console.log('buyer', buyersClient.account.address)
     console.log('seller', sellersClient.account.address)
 
